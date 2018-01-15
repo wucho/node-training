ae69e86f9d5ececb969045968d62cf24b1f0421f
JP <uszko70@gmail.com>
2018-01-15 04:15:08 +0100

    Changes for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 
 var app = express();
@@ -35,6 +36,6 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up....')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
